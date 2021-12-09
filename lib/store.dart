6c82e66125bee594d7414ce44441d319150d23d0
youngmin13 <21600620@handong.edu>
2021-12-09 23:18:16 +0900

    [Update] store.dart

--- lib/store.dart
+++ lib/store.dart
@@ -5,9 +5,18 @@ import 'store_detail.dart';
 
 class Store_id {
   final int store_id;
+  final double latitude;
+  final double longitude;
+  final String name;
+  final int review_count;
+  final String address_gu;
+  final String address;
+  final String phone;
+  final String image;
 
-  Store_id(this.store_id);
+  Store_id(this.store_id, this.latitude, this.longitude, this.name, this.review_count, this.address_gu, this.address, this.phone, this.image);
 }
+
 
 class Store extends StatefulWidget {
   const Store({Key? key}) : super(key: key);
@@ -19,114 +28,209 @@ class Store extends StatefulWidget {
 class _StoreState extends State<Store> {
   final List<String> valueList = ['북구・남구', '북구', '남구'];
   String dropdownValue = '북구・남구';
+  final TextEditingController _filter = TextEditingController();
+  Icon _searchIcon = const Icon(Icons.search);
+  Widget _appBarTitle = Text('Search Example');
+  List filteredNames = [];
+  List names = [];
+  String _searchText = "";
 
   bool total = true;
   bool Bukgu = true;
+  bool searching = false;
+
+  _StoreState() {
+    _filter.addListener(() {
+      if (_filter.text.isEmpty) {
+        setState(() {
+          _searchText = "";
+          filteredNames = names;
+        });
+      } else {
+        setState(() {
+          _searchText = _filter.text;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (!(_searchText.isEmpty)) {
+      List tempList = [];
+      for (int i = 0; i < filteredNames.length; i++) {
+        if (filteredNames[i]['name']
+            .toLowerCase()
+            .contains(_searchText.toLowerCase())) {
+          tempList.add(filteredNames[i]);
+        }
+      }
+      filteredNames = tempList;
+    }
     return DefaultTabController(
         length: 6,
         child: Scaffold(
-          appBar: AppBar(
-            toolbarHeight: 55,
-            backgroundColor: Colors.white,
-            iconTheme: const IconThemeData(
-              color: Colors.black,
+            appBar: AppBar(
+                toolbarHeight: 55,
+                backgroundColor: Colors.white,
+                iconTheme: const IconThemeData(
+                  color: Colors.black,
+                ),
+                // foregroundColor: Colors.black,
+                title: _appBarTitle,
+                actions: <Widget>[
+                  if (!searching)
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        border: Border.all(
+                            color: const Color(0xffc0e2af),
+                            style: BorderStyle.solid,
+                            width: 4),
+                      ),
+                      height: 10,
+                      width: 112,
+                      margin: const EdgeInsets.all(3),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          DropdownButton<String>(
+                            value: dropdownValue,
+                            icon: const Icon(Icons.arrow_downward,
+                                color: Color(0xffc0e2af)),
+                            iconSize: 22,
+                            style: const TextStyle(color: Colors.black),
+                            alignment: Alignment.center,
+                            underline: Container(),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownValue = newValue!;
+                                if (dropdownValue == '북구・남구') {
+                                  total = true;
+                                } else {
+                                  total = false;
+                                  if (dropdownValue == '남구') {
+                                    Bukgu = false;
+                                  } else {
+                                    Bukgu = true;
+                                  }
+                                }
+                              });
+                            },
+                            items: <String>['북구・남구', '북구', '남구']
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Center(child: Text(value)),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  IconButton(
+                    icon: _searchIcon,
+                    onPressed: () {
+                      _searchPressed();
+                    },
+                  ),
+                ],
+                // if (searching)
+                bottom: (searching
+                    ? const PreferredSize(
+                    preferredSize: Size.fromHeight(10),
+                    child: SizedBox(
+                      height: 10,
+                    ))
+                    : const PreferredSize(
+                  child: TabBar(
+                      isScrollable: true,
+                      indicatorColor: Color(0xffc0e2af),
+                      //unselectedLabelColor: Color(0x00c0e2af),
+                      labelColor: Colors.black,
+                      tabs: [
+                        Tab(
+                          text: '한식',
+                        ),
+                        Tab(text: '아시안・양식'),
+                        Tab(text: '중식'),
+                        Tab(text: '일식'),
+                        Tab(text: '분식'),
+                        Tab(text: '카페・디저트'),
+                      ]),
+                  preferredSize: Size.fromHeight(40),
+                ))),
+            body: (searching
+                ? _buildTabBarView('no', Bukgu, total, searching, _searchText)
+                : TabBarView(
+              children: [
+                _buildTabBarView(
+                    '한식', Bukgu, total, searching, _searchText),
+                _buildTabBarView(
+                    '양식', Bukgu, total, searching, _searchText),
+                _buildTabBarView(
+                    '중식', Bukgu, total, searching, _searchText),
+                _buildTabBarView(
+                    '일식', Bukgu, total, searching, _searchText),
+                _buildTabBarView(
+                    '분식', Bukgu, total, searching, _searchText),
+                _buildTabBarView(
+                    '카페', Bukgu, total, searching, _searchText),
+              ],
+            ))));
+  }
+
+  void _searchPressed() {
+    setState(() {
+      if (_searchIcon.icon == Icons.search) {
+        searching = true;
+        _searchIcon = const Icon(Icons.close);
+        _appBarTitle = TextField(
+          controller: _filter,
+          decoration: const InputDecoration(
+            prefixIcon: Icon(Icons.search, color: Color(0xffc0e2af)),
+            hintText: '메뉴 검색',
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Color(0xffc0e2af), width: 2),
             ),
-            // foregroundColor: Colors.black,
-            actions: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  border: Border.all(
-                      color: const Color(0xffc0e2af),
-                      style: BorderStyle.solid,
-                      width: 4),
-                ),
-                height: 10,
-                width: 105,
-                // alignment: Alignment.center,
-                margin: EdgeInsets.all(3),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    DropdownButton<String>(
-                      value: dropdownValue,
-                      icon: const Icon(Icons.arrow_downward),
-                      iconSize: 20,
-                      style: const TextStyle(color: Colors.black),
-                      alignment: Alignment.center,
-                      underline: Container(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          dropdownValue = newValue!;
-                          if (dropdownValue == '북구・남구') {
-                            total = true;
-                          } else {
-                            total = false;
-                            if (dropdownValue == '남구') {
-                              Bukgu = false;
-                            } else {
-                              Bukgu = true;
-                            }
-                          }
-                        });
-                      },
-                      items: <String>['북구・남구', '북구', '남구']
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Center(child: Text(value)),
-                        );
-                      }).toList(),
-                    ),
-                  ],
-                ),
-              )
-            ],
-            bottom: const PreferredSize(
-              child: TabBar(
-                  isScrollable: true,
-                  indicatorColor: Color(0xffc0e2af),
-                  //unselectedLabelColor: Color(0x00c0e2af),
-                  labelColor: Colors.black,
-                  tabs: [
-                    Tab(
-                      text: '한식',
-                    ),
-                    Tab(text: '아시안・양식'),
-                    Tab(text: '중식'),
-                    Tab(text: '일식'),
-                    Tab(text: '분식'),
-                    Tab(text: '카페・디저트'),
-                  ]),
-              preferredSize: Size.fromHeight(40),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Color(0xffc0e2af), width: 2),
             ),
           ),
-          body: TabBarView(
-            children: [
-              _buildTabBarView('한식', Bukgu, total),
-              _buildTabBarView('양식', Bukgu, total),
-              _buildTabBarView('중식', Bukgu, total),
-              _buildTabBarView('일식', Bukgu, total),
-              _buildTabBarView('분식', Bukgu, total),
-              _buildTabBarView('카페', Bukgu, total),
-            ],
-          ),
-        ));
+        );
+      } else {
+        _searchIcon = const Icon(Icons.search);
+        searching = false;
+        _appBarTitle = const Text('');
+        filteredNames = names;
+        _filter.clear();
+      }
+    });
   }
 }
 
-Widget _buildTabBarView(String category, bool Bukgu, bool total) {
+Widget _buildTabBarView(String category, bool Bukgu, bool total, bool searching,
+    String _searchText) {
   return StreamBuilder<QuerySnapshot>(
       stream: total
+          ? (searching
           ? (FirebaseFirestore.instance
           .collection('store')
-          .where('category', isEqualTo: category)
+      // .where('name', isEqualTo: _searchText)
+          .where('menu', arrayContains: _searchText)
           .snapshots())
           : (FirebaseFirestore.instance
+          .collection('store')
+          .where('category', isEqualTo: category)
+          .snapshots()))
+          : (searching
+          ? (FirebaseFirestore.instance
+          .collection('store')
+          .where('menu', arrayContains: _searchText)
+          .where('address_gu', isEqualTo: (Bukgu ? '북구' : '남구'))
+          .snapshots())
+          : FirebaseFirestore.instance
           .collection('store')
           .where('category', isEqualTo: category)
           .where('address_gu', isEqualTo: (Bukgu ? '북구' : '남구'))
@@ -139,10 +243,14 @@ Widget _buildTabBarView(String category, bool Bukgu, bool total) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Text("Loading");
         }
+
         return ListView(
           children: snapshot.data!.docs.map((DocumentSnapshot document) {
             Map<String, dynamic> data =
             document.data()! as Map<String, dynamic>;
+
+            String menu1 = data['menu'][0];
+            String menu2 = data['menu'][1];
             return Column(
               children: [
                 ListTile(
@@ -157,21 +265,11 @@ Widget _buildTabBarView(String category, bool Bukgu, bool total) {
                   subtitle: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Row(
-                        children: [
-                          Text(
-                            data['menu'][0],
-                            style: const TextStyle(fontSize: 11),
-                          ),
-                          const Text(
-                            ', ',
-                            style: TextStyle(fontSize: 11),
-                          ),
-                          Text(
-                            data['menu'][1],
-                            style: const TextStyle(fontSize: 11),
-                          ),
-                        ],
+                      Text(
+                        '$menu1 , $menu2',
+                        softWrap: true,
+                        // maxLines: 1,
+                        style: const TextStyle(fontSize: 12),
                       ),
                       const SizedBox(height: 10),
                       Text(data['business_time']),
@@ -182,12 +280,14 @@ Widget _buildTabBarView(String category, bool Bukgu, bool total) {
                         Navigator.pushNamed(
                           context,
                           StoreDetail.routeName,
-                          arguments: Store_id(data['store_id']),
+                          arguments: Store_id(data['store_id'], data['latitude'], data['longitude'], data['name'], data['review_count'], data['address_gu'], data['address'], data['phone'], data['image']),
                         );
                       },
                       icon: const Icon(Icons.navigate_next)),
                 ),
-                const Divider(),
+                const Divider(
+                  thickness: 1,
+                ),
               ],
             );
           }).toList(),
