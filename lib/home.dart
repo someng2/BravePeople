3fea6410eb9a998fcf080f723b426aa70c6757ec
somang-baek <21700336@handong.edu>
2021-12-10 18:01:54 +0900

    [UPDATE] home.dart 아이콘 변경

--- lib/home.dart
+++ lib/home.dart
@@ -29,6 +29,7 @@ class _HomePageState extends State<HomePage> {
     });
     final ThemeData theme = Theme.of(context);
     return Scaffold(
+      // backgroundColor: const Color(0xffF3FAEF),
       body: SafeArea(
         child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -50,7 +51,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                     const SizedBox(width: 40.0),
                     IconButton(
-                      icon: Icon(Icons.menu, color: Color(0xff13740B)),
+                      icon: const Icon(Icons.menu, color: Color(0xff13740B)),
                       onPressed: () {
                         Navigator.pushNamed(context, '/more', arguments: Username(username));
                       },
@@ -78,49 +79,51 @@ class _HomePageState extends State<HomePage> {
                   child: Column(
                     children: <Widget>[
                       IconButton(
-                        padding: EdgeInsets.fromLTRB(0, 10, 75, 0),
-                          icon: Icon(Icons.restaurant, size: 100),
+                        padding: EdgeInsets.fromLTRB(20, 10, 75, 0),
+                          icon: Icon(Icons.restaurant, size: 80),
                           onPressed: () {
                             //제휴점으로 가야함
                             Navigator.pushNamed(context, '/store');
                           },
                       ),
-                      const SizedBox(height: 100.0),
+                      const SizedBox(height: 90.0),
                       const SizedBox(width: 120.0),
-                      Text('제휴점',
+                      const Text('제휴점',
                           textAlign: TextAlign.center,
                           style: TextStyle(
                             fontSize: 25,
                           ),
                       ),
+                      const SizedBox(height: 10.0),
                     ],
                   ),
                 ),
                 //const SizedBox(width: 60.0),
                 Container(
                   decoration: BoxDecoration(
-                    color: Color(0xffDBEE91),
-                    border: Border.all(width: 10, color: Color(0xffdbee91)),
-                    borderRadius: const BorderRadius.all(const Radius.circular(8)),
+                    color: const Color(0xffDBEE91).withOpacity(0.8),
+                    border: Border.all(width: 10, color: const Color(0xffdbee91).withOpacity(0.8),),
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
                   ),
                   child: Column(
                     children: <Widget>[
                       IconButton(
-                        padding: EdgeInsets.fromLTRB(0, 10, 75, 0),
-                          icon: Icon(Icons.people, size: 100, color: Colors.pinkAccent),
+                        padding: EdgeInsets.fromLTRB(20, 10, 75, 0),
+                          icon: Icon(Icons.people, size: 80, color: Colors.pinkAccent),
                           onPressed: () {
                             //커뮤니티로 가야함
                             Navigator.pushNamed(context, '/communityMain');
                           },
                       ),
-                      const SizedBox(height: 100.0),
+                      const SizedBox(height: 90.0),
                       const SizedBox(width: 120.0),
-                      Text('커뮤니티',
+                      const Text('커뮤니티',
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           fontSize: 25,
                         ),
                       ),
+                      const SizedBox(height: 10.0),
                     ],
                   ),
                 ),
@@ -129,82 +132,168 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(height: 40.0),
             Container(
               decoration: BoxDecoration(
-                color: Color(0xffD0E5BD),
-                border: Border.all(width: 10, color: Color(0xffD0E5BD)),
-                borderRadius: const BorderRadius.all(const Radius.circular(8)),
+                color: Colors.white,
+                border: Border.all(width: 5, color: Color(0xffC0E2AF)),
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
               ),
-              child: Row(
-                children: <Widget> [
-                  Column(
-                    children: <Widget> [
-                      IconButton(
-                        padding: EdgeInsets.fromLTRB(20, 10, 60, 20),
-                        icon: Icon(Icons.wallet_giftcard, size: 60, color: Colors.lightBlueAccent),
-                        onPressed: () {
-                          //쿠폰으로 가야함
-                          Navigator.pushNamed(context, '/coupon');
-                        },
-                      ),
-                      const SizedBox(height: 20.0),
-                      Text('쿠폰',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 15,
+
+              child: Center(
+                child: SizedBox(
+                  height: 120,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              TextButton(
+                                // padding: EdgeInsets.fromLTRB(30, 15, 50, 20),
+                                // icon: Icon(Icons.thumb_up, size: 60, color: Colors.yellowAccent),
+                                //   child: Image.asset('assets/like.png', height: 90, width: 10),
+                                child: Image.asset('assets/gift.png',
+                                    fit: BoxFit.contain, height: 75),
+                                onPressed: () {
+                                  //나의 리뷰로 가야
+                                  Navigator.pushNamed(context, '/coupon');
+                                },
+                              ),
+                              const SizedBox(height: 7),
+                              const Text(
+                                '쿠폰',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                          flex: 1,
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(width: 30.0),
-                  Column(
-                    children: <Widget> [
-                      IconButton(
-                        padding: EdgeInsets.fromLTRB(0, 10, 40, 20),
-                        icon: Icon(Icons.favorite, size: 60, color: Colors.red),
-                        onPressed: () {
-                          //찜 목록으로 가야함
-                          Navigator.pushNamed(context, '/favorite');
-                        },
-                      ),
-                      const SizedBox(height: 20.0),
-                      Text('찜',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 15,
+                        Expanded(
+                          child: Column(
+                            children: [
+                              TextButton(
+                                // padding: const EdgeInsets.fromLTRB(0, 15, 40, 20),
+                                child: Image.asset('assets/favorite.png',
+                                    fit: BoxFit.contain, height: 65),
+                                onPressed: () {
+                                  //찜 목록으로 가야함
+                                  Navigator.pushNamed(context, '/favorite');
+                                },
+                              ),
+                              const SizedBox(height: 12),
+                              const Text(
+                                '찜',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                          flex: 1,
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(width: 20.0),
-                  Column(
-                    children: <Widget> [
-                      IconButton(
-                        padding: EdgeInsets.fromLTRB(30, 10, 50, 20),
-                        icon: Icon(Icons.thumb_up, size: 60, color: Colors.yellowAccent),
-                        onPressed: () {
-                          //나의 리뷰로 가야함
-                          Navigator.pushNamed(context, '/myReview');
-                        },
-                      ),
-                      const SizedBox(height: 20.0),
-                      Text('나의 리뷰',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 15,
+                        Expanded(
+                          child: Column(
+                            children: [
+                              TextButton(
+                                // padding: EdgeInsets.fromLTRB(30, 15, 50, 20),
+                                // icon: Icon(Icons.thumb_up, size: 60, color: Colors.yellowAccent),
+                                //   child: Image.asset('assets/like.png', height: 90, width: 10),
+                                child: Image.asset('assets/review.png',
+                                    fit: BoxFit.contain, height: 80),
+                                onPressed: () {
+                                  //나의 리뷰로 가야
+                                  Navigator.pushNamed(context, '/myReview');
+                                },
+                              ),
+                              const Text(
+                                '나의 리뷰',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                          flex: 1,
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ]),
+                ),
               ),
+              // child: Row(
+              //   children: <Widget> [
+              //     Column(
+              //       children: <Widget> [
+              //         IconButton(
+              //           padding: EdgeInsets.fromLTRB(20, 10, 60, 20),
+              //           icon: Icon(Icons.wallet_giftcard, size: 60, color: Colors.lightBlueAccent),
+              //           onPressed: () {
+              //             //쿠폰으로 가야함
+              //             Navigator.pushNamed(context, '/coupon');
+              //           },
+              //         ),
+              //         const SizedBox(height: 20.0),
+              //         Text('쿠폰',
+              //           textAlign: TextAlign.center,
+              //           style: TextStyle(
+              //             fontSize: 15,
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //     const SizedBox(width: 30.0),
+              //     Column(
+              //       children: <Widget> [
+              //         IconButton(
+              //           padding: EdgeInsets.fromLTRB(0, 10, 40, 20),
+              //           icon: Icon(Icons.favorite, size: 60, color: Colors.red),
+              //           onPressed: () {
+              //             //찜 목록으로 가야함
+              //             Navigator.pushNamed(context, '/favorite');
+              //           },
+              //         ),
+              //         const SizedBox(height: 20.0),
+              //         Text('찜',
+              //           textAlign: TextAlign.center,
+              //           style: TextStyle(
+              //             fontSize: 15,
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //     const SizedBox(width: 20.0),
+              //     Column(
+              //       children: <Widget> [
+              //         IconButton(
+              //           padding: EdgeInsets.fromLTRB(30, 10, 50, 20),
+              //           icon: Icon(Icons.thumb_up, size: 60, color: Colors.yellowAccent),
+              //           onPressed: () {
+              //             //나의 리뷰로 가야함
+              //             Navigator.pushNamed(context, '/myReview');
+              //           },
+              //         ),
+              //         const SizedBox(height: 20.0),
+              //         Text('나의 리뷰',
+              //           textAlign: TextAlign.center,
+              //           style: TextStyle(
+              //             fontSize: 15,
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //   ],
+              // ),
             ),
             const SizedBox(height: 30.0),
             Container(
               decoration: BoxDecoration(
-                color: Color(0xffC0E2AF),
-                border: Border.all(width: 10, color: Color(0xffC0E2AF)),
+                color: Color(0xffC0E2AF).withOpacity(0.2),
+                border: Border.all(width: 10, color: Color(0xffC0E2AF).withOpacity(0.2)),
                 borderRadius: const BorderRadius.all(const Radius.circular(8)),
               ),
-              child: Text(
+              child: const Text(
                 'MobileAppDevelopment',
                 style: TextStyle(
                   fontSize: 15,
